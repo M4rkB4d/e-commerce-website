794c2a11078edcef1bd80e7198d38524117769c6
M4rkB4d <87140625+M4rkB4d@users.noreply.github.com>
2024-12-10 13:26:56 +0800

    add initial files

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import ItemDisplayPage from './pages/ItemDisplayPage';
+import LoginPage from './pages/LoginPage';
+
+import HelloWorld from './components/HellowWorld';
+
+function App(): JSX.Element {
+  return (
+    <div className='App'>
+      <Router>
+      <div>
+        {/* Navigation */}
+        <nav style={{ padding: '10px', backgroundColor: '#f8f8f8', marginBottom: '20px' }}>
+          <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+          <Link to="/items" style={{ marginRight: '10px' }}>Items</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<HelloWorld name={'Mother Fucker'}></HelloWorld>} />
+          <Route path="/items" element={<ItemDisplayPage />} />
+          <Route path='/login' element={<LoginPage /> } />
+        </Routes>
+      </div>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
